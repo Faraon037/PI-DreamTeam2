895b9abd4507fe14c0e111e8e004728a1ade3010
MariaBelyaeva1 <130570513+MariaBelyaeva1@users.noreply.github.com>
2023-04-28 00:17:30 +0300

    Add files via upload

---
+++ Var_4.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.header('Вариант 4. Выполнила Беляева М.М., группа 3-см')
+st.subheader("Задание:")
+st.info("Вывести имена пассажиров, стоимость билета которых была выше указанной")
+st.subheader("Результат:")
+cena = st.text_input("Введите стоимость билета", 10000)
+with open("data.csv") as file:
+    for line in file:
+        if line.split(",")[0] == "PassengerId":
+            continue
+        if float(line.split(",")[10]) > float(cena):
+            fare = (line.split(",")[10])
+            name = line.split(",")[3] + line.split(",")[4]
+            res  = (name[1:-1] + ", " + "Цена билета " + "- " + fare)
+            st.text(res)
