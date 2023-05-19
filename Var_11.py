174fda0ec4a6351e49f0bd14330904f4c43df701
Kseniya <gipsyss@mail.ru>
2023-05-20 02:02:16 +0700

    editing code for lab 12

--- Var_11.py
+++ Var_11.py
@@ -1,34 +1,36 @@
 import streamlit as st
 
 
-def var11():
-    avr_dead = 0
-    avr_alive = 0
-    sum_dead = 0
-    sum_alive = 0
-    count_dead = 0
-    count_alive = 0
-    with open('data.csv') as file:
-        for line in file:
-            str=line.split(',')
-            if str[1] == '0':
-                count_dead += 1
-                sum_dead += float(str[10])
-            elif str[1] == '1':
-                count_alive += 1
-                sum_alive += float(str[10])
-        avr_dead = sum_dead/count_dead
-        avr_alive = sum_alive/count_alive
+def var11(file, attribute):
+    avr_dead_alive = 0
+    sum_dead_alive = 0
+    count_dead_alive = 0
+    for line in file:
+        str = line.split(',')
+        if str[1] == attribute:
+            count_dead_alive += 1
+            sum_dead_alive += float(str[10])
+    avr_dead_alive = sum_dead_alive / count_dead_alive
+    return avr_dead_alive
+
+
+def run_result():
+    file_data = open('data.csv', 'r')
+
     st.header('Вариант 11. Выполнила Григорьева К.В., группа 3-см')
     st.subheader("Задание:")
-    st. info('Посчитать среднюю стоимость билета (поле Fare) у пассажиров, указав - спасен или нет.')
+    st.info('Посчитать среднюю стоимость билета (поле Fare) у пассажиров, указав - спасен или нет.')
     st.subheader("Результат:")
-    choice = st.radio ('Показатель везения пассажира', options = ['Спасен', 'Погиб'])
+
+    choice = st.radio('Показатель везения пассажира', options=['Спасен', 'Погиб'])
     if choice == 'Спасен':
-        avr_out = avr_alive
+        avr_out = var11(file_data, '1')
         text_out = 'Средняя стоимость билета спасеннных пассажиров'
     elif choice == 'Погиб':
-        avr_out = avr_dead
+        avr_out = var11(file_data, '0')
         text_out = 'Средняя стоимость билета погибших пассажиров'
     st.text(text_out)
     st.success(avr_out)
+
+
+run_result()
