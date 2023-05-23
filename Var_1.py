9f1df82915d6c90e37f5ba29a30ce81a18021cad
AleksandrovaEvgenia <130659110+AleksandrovaEvgenia@users.noreply.github.com>
2023-05-23 20:06:26 +0700

    Update Var_1.py
    Правка кода для 12 и 13 ЛР

--- Var_1.py
+++ Var_1.py
@@ -1,28 +1,31 @@
 import streamlit as st
 
 
+def get_result(data, sex_need, age_min, age_max):
+    titanic = {}
+    for line in data:
+        lst = line.strip().split(',')
+        name = lst[3] + " " + lst[4]
+        sex = lst[5]
+        age = lst[6]
+        if age == 'SibSp':
+            continue
+        if age and (float(age) >= float(age_min)) and (float(age) <= float(age_max)) and sex == sex_need:
+            passenger_info = {"sex": sex, "age": age}
+            titanic[name] = passenger_info
+    return titanic
+
+
 def var1():
+    a = open("data.csv", "r")
     st.header('Вариант 1. Выполнила Александрова Е.В., группа 3-см')
     st.subheader("Задание:")
-    st. info('Вывести имя и возраст спасенных детей, указав пол и возраст (от 0 до 18)')
+    st.info('Вывести имя и возраст спасенных детей, указав пол и возраст (от 0 до 18)')
     st.subheader("Результат:")
     sex_select = st.selectbox("Выберите пол пассажира", ('male', 'female'))
     age_select = st.slider("Укажите возрастной диапазон пассажиров", 0, 18, [1, 10])
-    age_min = age_select[0]
-    age_max = age_select[1]
-
-    titanic = {}
-    with open("data.csv") as data_file:
-        next(data_file)
-        for line in data_file:
-            lst = line.strip().split(',')
-
-            name = lst[3] + lst[4]
-            sex = lst[5]
-            age = lst[6]
-
-            if age and (float(age) >= float(age_min)) and (float(age) <= float(age_max)) and sex == sex_select:
-                passenger_info = {"sex": sex, "age": age}
-                titanic[name[1: -1]] = passenger_info
-    #sex == "male" and age and
-    st.table(titanic)
+    min_age = age_select[0]
+    max_age = age_select[1]
+    result = get_result(a, sex_select, min_age, max_age)
+    st.table(result)
+    
