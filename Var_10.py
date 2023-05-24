97fc298b1721b20f75ecf87c94ca4b8b1845aae3
Faraon037 <130658862+Faraon037@users.noreply.github.com>
2023-05-24 12:30:14 +0300

    Update Var_10.py

--- Var_10.py
+++ Var_10.py
@@ -1,19 +1,20 @@
 import streamlit as st
 
 
-def var10():
-    def sex_deadh (data, sex):
-        pas_i = 0
-        i_all = 0
-        for line in data:
-            lst = line.rstrip().split(',')
-            if lst[1] == "1" and lst[5] == sex:
-                pas_i += 1
-                i_all += 1
-            elif lst[1] == "0" and lst[5] == sex:
-                i_all += 1
-        return round((pas_i / i_all) * 100, 2)
 
+def sex_deadh (data, sex):
+    pas_i = 0
+    i_all = 0
+    for line in data:
+        lst = line.rstrip().split(',')
+        if lst[1] == "1" and lst[5] == sex:
+            pas_i += 1
+            i_all += 1
+        elif lst[1] == "0" and lst[5] == sex:
+            i_all += 1
+    return round((pas_i / i_all) * 100, 2)
+
+def var10():
     st.title('Приложение "Титаник"')
     st.header("Вариант №10 \n Выполнил Кузнецов Н.В. гр. 3 см")
     st.subheader("Задание \n Посчитать долю выживших среди пассажиров указанного пола.")
