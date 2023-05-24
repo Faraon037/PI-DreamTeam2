342f3b2a24ac40be2e231c5d83f51f90c767f85d
Faraon037 <130658862+Faraon037@users.noreply.github.com>
2023-05-24 13:25:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ import Var_11
 import Var_10
 with open("data.csv") as file:
     data = file.readlines()
-    st.title('Веб-приложение "ТИТАНИК" (разработка PI-DreamTeam2)')
-    option = st.selectbox('Выберите исполнителя',
-        ('Григорьева К.В.', 'Александрова Е.В', 'Кузнецов Н.В.', 'Беляева М.М.', 'Бессонова А.А.'))
-    if option == 'Григорьева К.В.':
-        Var_11.run_result()
-    if option == 'Александрова Е.В':
-        Var_1.var1()
-    if option == 'Кузнецов Н.В.':
-        Var_10.var10()    
-    if option == 'Беляева М.М.':
-        Var_4.var4() 
-    if option == 'Бессонова А.А.':
-        Var_5_2.var5_2()   
+st.title('Веб-приложение "ТИТАНИК" (разработка PI-DreamTeam2)')
+option = st.selectbox('Выберите исполнителя',
+    ('Григорьева К.В.', 'Александрова Е.В', 'Кузнецов Н.В.', 'Беляева М.М.', 'Бессонова А.А.'))
+if option == 'Григорьева К.В.':
+   Var_11.run_result()
+if option == 'Александрова Е.В':
+    Var_1.var1()
+if option == 'Кузнецов Н.В.':
+    Var_10.var10(data)    
+if option == 'Беляева М.М.':
+    Var_4.var4() 
+if option == 'Бессонова А.А.':
+    Var_5_2.var5_2()   
