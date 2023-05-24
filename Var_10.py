c19988ae5fad9c1a1312f9f72f5116dee6547b2d
Faraon037 <130658862+Faraon037@users.noreply.github.com>
2023-05-24 11:32:05 +0300

    Update Var_10.py

--- Var_10.py
+++ Var_10.py
@@ -2,20 +2,24 @@ import streamlit as st
 
 
 def var10():
-def sex_deadh (data, sex):
-    pas_i = 0
-    i_all = 0
-    res = float(0)
-    for line in data:
-        lst = line.rstrip().split(',')
-        if lst[1] == "1" and lst[5] == sex:
-            pas_i += 1
-            i_all += 1
-        elif lst[1] == "0" and lst[5] == sex:
-            i_all += 1
-    res = round((pas_i / i_all) * 100,2)
-    return res
+    def sex_deadh (data, sex):
+        pas_i = 0
+        i_all = 0
+        res = float(0)
+        for line in data:
+            lst = line.rstrip().split(',')
+            if lst[1] == "1" and lst[5] == sex:
+                pas_i += 1
+                i_all += 1
+            elif lst[1] == "0" and lst[5] == sex:
+                i_all += 1
+        res = round((pas_i / i_all) * 100,2)
+        return res
 
-with open("data.csv") as file:
-    data = file.readlines()
-print(sex_deadh(data,"male"))
+    st.title('Приложение "Титаник"')
+    st.header("Вариант №10 \n Выполнил Кузнецов Н.В. гр. 3 см")
+    st.subheader("Задание \n Посчитать долю выживших среди пассажиров указанного пола.")
+    with open("data.csv") as file:
+        data = file.readlines()
+        sex = st.selectbox("Пол выживших пассажиров", ["Мужской", "Женский"])
+    print(sex_deadh(data,"male"))
