d0cd3488785c7419c2227b453dc82d0c3964dafd
Faraon037 <130658862+Faraon037@users.noreply.github.com>
2023-05-25 12:36:17 +0300

    Update var_4.py

--- var_4.py
+++ var_4.py
@@ -5,12 +5,13 @@ def name_cena(data,cena):
     name = []
     name_out = []
     for line in data:
+        lst = line.rstrip().split(',')
         if line.split(",")[0] == "PassengerId":
             continue
         if float(line.split(",")[10]) > float(cena):
-            name = line.split(",")[3] + line.split(",")[4]
-            name_out += name
-    return name_out
+            name = lst[3] + lst[4]
+            name_out += [n[1:-1]]
+        return name_out
 
 def var4(data):
     st.header('Вариант 4. Выполнила Беляева М.М., группа 3-см')
