77aa8d4782286e3ad6ff536cecf81405effed985
Faraon037 <130658862+Faraon037@users.noreply.github.com>
2023-05-25 14:16:40 +0300

    Update var_4.py

--- var_4.py
+++ var_4.py
@@ -20,4 +20,5 @@ def var4(data):
     st.info("Вывести имена пассажиров, стоимость билета которых была выше указанной")
     st.subheader("Результат:")
     cena = st.text_input("Введите стоимость билета", 10000)
-    st.table(name_cena(data, cena))
+    for line in data:
+        st.text(name_cena(data, cena))
