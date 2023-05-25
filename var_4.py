bf66c3706ff9c02e26e4aad4a6a88e19641d5e8f
Faraon037 <130658862+Faraon037@users.noreply.github.com>
2023-05-25 13:11:12 +0300

    Update var_4.py

--- var_4.py
+++ var_4.py
@@ -9,7 +9,7 @@ def name_cena(data,cena):
         if float(lst[10])  > float(cena):
             n = lst[3] + lst[4]
             x = ", ".join([lst[2], n[1:-1], lst[6]])
-        return x
+        return n
 #        if float(line.split(",")[10]) > float(cena):
 #           name = [lst[3] + lst[4]]
  #           name_out = ", ".join(name[1:-1])
