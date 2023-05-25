5d99f4127d218554625dc741d2de1779a6dc4db5
Faraon037 <130658862+Faraon037@users.noreply.github.com>
2023-05-25 14:34:47 +0300

    Update test_var4.py

--- test_var4.py
+++ test_var4.py
@@ -5,7 +5,7 @@ def test_10():
     data = ["0,1,2,Test,Test,5,6,7,8,9,30", "1,1,2,Test,Test,5,6,7,8,9,60", "2,1,2,Test,Test,5,6,7,8,9,90"]
     assert name_cena(data, 10) == ["TestTest", "TestTest", "TestTest"]
 
-    
+
 def test_50():
     data = ["0,1,2,Test,Test,5,6,7,8,9,45", "1,1,2,Test,Test,5,6,7,8,9,60", "2,1,2,Test,Test,5,6,7,8,9,20"]
     assert name_cena(data, 50) == ["TestTest"]
