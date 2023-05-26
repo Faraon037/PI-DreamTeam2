30a35cda698abcbd57058381c509c177ccba4935
Faraon037 <130658862+Faraon037@users.noreply.github.com>
2023-05-26 21:13:57 +0300

    Add files via upload

---
+++ test_var5.py
@@ -0,0 +1,11 @@
+from var_5 import var5
+
+def test_alive_iv():
+    data = ['0,1,2,"Ivanov,Out"', '1,1,2,"Petrov,Out"', '2,1,1,"Ivan,Out"']
+    assert var5(data, 1, "Iva") == ['"IvanovOut"','"IvanOut"']
+
+
+def test_deatha_ru():
+    data = ['0,0,2,"Ruanor,Out"', '1,0,2,"Rutrov,Out"', '2,1,1,"Ribov,Out"']
+    assert var5(data, 0, "ru") == ['"RuanorOut"','"RutrovOut"']
+
---
+++ var_5.py
@@ -0,0 +1,21 @@
+
+def var5(data,save,name):
+#    st.header("Вариант 5. Выполнила Бессонова А.А., группа 3-см")
+#    st.info("Вывести Класс, Имя, Возраст спасенных пассажиров с именами, начинающимися на введенный текст")
+#    st.subheader("Результат:")
+#    name = st.text_input("Введите первые буквы имени спасенного пассажира")
+    n = []
+    y = []
+    ln_name = len(name)
+    for line in data:
+        lst = line.rstrip().split(',')
+        fio = lst[3] + lst[4]
+        fio1 = fio[1:ln_name + 1]
+        if fio1 == name.capitalize() and int(lst[1]) == int(save) and ln_name > 0:
+            y += [fio]
+    return y
+
+
+with open("data.csv") as file:
+    data = file.readlines()
+print(var5(data, 0, "br"))
