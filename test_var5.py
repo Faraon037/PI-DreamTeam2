86327bcc0151bee2b0dc0d2489b47204a5a15917
Faraon037 <130658862+Faraon037@users.noreply.github.com>
2023-05-26 21:41:27 +0300

    Update test_var5.py

--- test_var5.py
+++ test_var5.py
@@ -2,10 +2,10 @@ from var_5 import pas_save
 
 def test_alive_iv():
     data = ['0,1,2,"Ivanov,Out"', '1,1,2,"Petrov,Out"', '2,1,1,"Ivan,Out"']
-    assert var5(data, "Iva") == ['"IvanovOut"','"IvanOut"']
+    assert pas_save(data, "Iva") == ['"IvanovOut"','"IvanOut"']
 
 
 def test_alive_ru():
     data = ['0,1,2,"Ruanor,Out"', '1,1,2,"Rutrov,Out"', '2,0,1,"Rubov,Out"']
-    assert var5(data, "ru") == ['"RuanorOut"','"RutrovOut"']
+    assert pas_save(data, "ru") == ['"RuanorOut"','"RutrovOut"']
 
