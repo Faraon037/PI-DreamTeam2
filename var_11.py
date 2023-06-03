58cb1416dca68b0a60e299dbca9467c3144067b8
GrigorevaKsenia <130657564+GrigorevaKsenia@users.noreply.github.com>
2023-06-03 23:57:14 +0700

    Update var_11.py
    Внесение изменений в код и добавление комментария по заданию преподавателя.

--- var_11.py
+++ var_11.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-def var11(file, attribute): # Основной код
+def var11(file, attribute):
     avr_dead_alive = 0
     sum_dead_alive = 0
     count_dead_alive = 0
@@ -14,9 +14,12 @@ def var11(file, attribute): # Основной код
     return avr_dead_alive
 
 
+# Сим комментарием доказываю, что я, Григорьева Ксения, сама внесла изменения в данный файл в 23:53 по Красноярску 03.06.2023.
+
 def run_result(data):
 
-    st.header('Вариант 11. Выполнила Григорьева К.В., группа 3-см')
+    st.header('Вариант 11')
+    st.header('Выполнила Григорьева К.В., группа 3-см')
     st.subheader("Задание:")
     st.info('Посчитать среднюю стоимость билета (поле Fare) у пассажиров, указав - спасен или нет.')
     st.subheader("Результат:")
